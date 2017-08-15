ed3fac3be3242134b86d2859581a2550fe5f2cd0
Juan José Eraso Escalona <juanjo.xae@gmail.com>
2017-08-15 20:08:58 +0200

    start up file, loads cal/aoboa and slc

---
+++ load.pl
@@ -0,0 +1,17 @@
+%%%
+%%% load.pl
+%%%
+%%% slc
+%%%   set up file
+%%%
+%%%
+%%% (c) 2017, xae. Juan José Eraso Escalona
+%%% 20170815
+%%%
+
+%% Parents support
+:- ensure_loaded('aoboa.pl').
+
+%% Calling prolog terms support
+:- ensure_loaded('cal.pl').
+
